413069b96f9f22d22841498f61bbfc726a10d41d
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-18 21:58:45 +0100

    make release_bike method return new Bike object.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,11 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
-    it 'responds to release bike' do
-        expect(subject).to respond_to :release_bike
+    it { is_expected.to respond_to :release_bike }
+
+    it 'releases working bikes' do
+        bike = subject.release_bike
+        expect(bike).to be_working
     end
 end
 	
