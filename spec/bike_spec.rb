e550539e95bf292cf43e9c309c1d8dd454425a55
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-19 13:22:50 +0100

    Add report_broken method to Bike to allow user to set change working attribute.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,23 @@ require './lib/bike.rb'
 
 describe Bike do
     it { is_expected.to respond_to :working? }
+
+    describe '#working?' do
+        it 'returns true if the bike is working' do
+            expect(subject.working?).to eq true
+        end
+
+        it 'returns false if the bike is not working' do
+            subject.report_broken
+            expect(subject.working?).to eq false
+        end
+    end
+
+    describe '#report_broken' do
+        it "allows a user to report a bike as not working" do
+            subject.report_broken
+            expect(subject.working?).to eq false
+        end    
+    end
+
 end
