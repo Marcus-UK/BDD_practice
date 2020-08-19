979d24e68da7d2f2c37cb87c84130cd8ad9f3ad9
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-19 12:59:41 +0100

    Add ability for users to set their own DockingStation capacity.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,16 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
     it { is_expected.to respond_to :bikes }
+
+    it 'has a default capacity if the user does not provide their own' do
+        expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    end
+
+    it 'allows users to set their own capacity' do
+        custom_capacity = 20
+        docking_station = DockingStation.new(capacity: custom_capacity)
+        expect(subject.capacity).to eq custom_capacity
+    end
      
     describe '#release_bike' do
         it 'releases working bikes' do
